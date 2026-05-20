10dd8b43aa2d07d787e2562b463f9dd23d442eca
luismedina-us <luis.medi1811@gmail.com>
2026-05-20 17:19:58 -0300

    Implement movie creation system

---
+++ dao/movieDAO.php
@@ -0,0 +1,83 @@
+<?php 
+
+ require_once("models/Movie.php");
+ require_once("models/Message.php");
+
+ // Review DAO
+
+ class MovieDAO implements MovieDAOInterface{
+
+    private $conn;
+    private $url;
+    private $message;
+
+    public function __construct(PDO $conn, $url) {
+        $this->conn = $conn;
+        $this->url = $url;
+        $this->message = new Message($url);
+    }
+    
+    public function buildMovie($data){
+
+    $movie = new Movie();
+            
+            $movie->id = $data["id"];
+            $movie->title = $data["title"];
+            $movie->description = $data["description"];
+            $movie->image = $data["image"];
+            $movie->trailer = $data["trailer"];
+            $movie->category = $data["category"];
+            $movie->length = $data["length"];
+            $movie->user_id = $data["user_id"];
+
+            return $movie;
+
+    }
+    public function findAll(){
+
+    }
+    public function getLatesMovies(){
+
+    }
+    public function getMoviesByCategory($category){
+
+    }
+    public function getMoviesByUserId($id){
+
+    }
+    public function findById($id){
+
+    }
+    public function findByTitle($title){
+
+    }
+    public function create(Movie $movie){
+
+        $stmt = $this->conn->prepare("INSERT INTO movies (
+            title, description, image, trailer, category, length, user_id 
+        ) VALUES (
+            :title, :description, :image, :trailer, :category, :length, :user_id
+        )");
+
+        $stmt->bindParam(":title", $movie->title);
+        $stmt->bindParam(":description", $movie->description);
+        $stmt->bindParam(":image", $movie->image);
+        $stmt->bindParam(":trailer", $movie->trailer);
+        $stmt->bindParam(":category", $movie->category);
+        $stmt->bindParam(":length", $movie->length);
+        $stmt->bindParam(":user_id", $movie->user_id);
+
+        $stmt->execute();
+
+        // Add movie success message 
+        $this->message->setMessage("Movie add successfully","success", "index.php");
+
+    }
+    public function update(Movie $movie){
+
+    }
+    public function destroy($id){
+
+    }
+
+ }
---
+++ movieprocess.php
@@ -0,0 +1,78 @@
+<?php
+
+    require_once("globals.php");
+    require_once("db.php");
+    require_once("models/Movie.php");
+    require_once("models/Message.php");
+    require_once("dao/userDAO.php");
+    require_once("dao/movieDAO.php");
+
+    $message = new Message($BASE_URL);
+    $userDAO = new UserDAO($conn, $BASE_URL);
+    $movieDAO = new MovieDAO($conn, $BASE_URL);
+
+    // Rescue the form type
+    $type = filter_input(INPUT_POST, "type");
+
+    // Rescue user data
+    $userData = $userDAO->verifyToken();
+
+    if($type === "create"){
+
+        // Recive inputs data
+        $title = filter_input(INPUT_POST, "title");
+        $description = filter_input(INPUT_POST, "description");
+        $trailer = filter_input(INPUT_POST, "trailer");
+        $category = filter_input(INPUT_POST, "category");
+        $length = filter_input(INPUT_POST, "length");
+
+        $movie = new Movie();
+
+        // Data verification 
+        if(!empty($title) && !empty($description) && !empty($category)){
+
+            $movie->title = $title;
+            $movie->description = $description;
+            $movie->trailer = $trailer;
+            $movie->category = $category;
+            $movie->length = $length;
+            $movie->user_id = $userData->id;
+
+            // Movie image upload
+            if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])){
+
+                $image = $_FILES["image"];
+                $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
+                $jpgArray = ["image/jpeg", "image/jpg"];
+
+                // Check image type
+                if(in_array($image["type"], $imageTypes)){
+
+                    // Check image jpg type
+                    if(in_array($image["type"], $jpgArray)){
+                        $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+                    } else {
+                        $imageFile = imagecreatefrompng($image["tmp_name"]);
+                    }
+                    
+                    // Generate image name
+                    $imageName = $movie->imageGenerateName();
+
+                    imagejpeg($imageFile, "./img/movies/" . $imageName, 100);
+
+                    $movie->image = $imageName;
+
+                } else {
+                    $message->setMessage("Invalid image type", "error", "back");
+                }
+            }
+
+            $movieDAO->create($movie);
+
+        } else {
+            $message->setMessage("Please fill in all required fields: title, description, and category.", "error", "back");
+        }
+
+    } else {
+        $message->setMessage("Invalid data provided!", "error", "index.php");
+    }
