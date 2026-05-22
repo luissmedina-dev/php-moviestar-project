f73f44f627b7fdc4b7e415793352cd40624e98d0
luismedina-us <luis.medi1811@gmail.com>
2026-05-21 22:00:46 -0300

    view movie page

--- movie.php
+++ movie.php
@@ -29,6 +29,11 @@
         }
     }
 
+    // Check if movie have image
+    if($movie->image === ""){
+        $movie->image = "movie_cover.jpg";
+    }
+
     // Check if the movie belongs to the user
     $userOwnsMovie = false; 
 
@@ -42,4 +47,113 @@
 
     // Rescue movie reviews
 
+?>
+
+<div id="main-container" class="container-fluid">
+    <div class="row">
+        <div class="offset-md-1 col-md-6 movie-container">
+            <h1 class="page-title"><?= $movie->title ?></h1>
+            <p class="movie-details">
+                <span>Runtime: <?= $movie->length ?></span>
+                <span class="pipe"></span>
+                <span><?= $movie->category ?></span>
+                <span class="pipe"></span>
+                <span><i class="fas fa-star"></i>9</span>
+            </p>
+            <iframe src="<?= $movie->trailer ?>" width="560" height="315" frameboarder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p><?= $movie->description ?></p>
+        </div>
+        <div class="col-md-4">
+            <div class="movie-image-container" style="background-image: url('<?= $BASE_URL ?>img/movies/<?= $movie->image ?>')"></div>
+        </div>
+        <div class="offset-md-1 col-md-10" id="reviews-container">
+            <h3 id="reviews-title">Reviews</h3>
+            <!-- Check if the user can submit reviews -->
+             <div class="col-md-12" id="review-form-container">
+                <h4>Submit your review:</h4>
+                <p class="page-description">Fill out the form with rating and review</p>
+                <form action="<?= $BASE_URL ?>reviewprocess.php" id="review-form" method="post">
+                    <input type="hidden" name="type" value="create">
+                    <input type="hidden" name="movies_id" id="<?= $movie->id ?>">
+                    <div class="form-group">
+                        <label for="rating">Movie rating</label>
+                        <select name="rating" id="rating" class="form-control">
+                            <option value="">Select</option>
+                            <option value="10">10</option>
+                            <option value="9">9</option>
+                            <option value="8">8</option>
+                            <option value="7">7</option>
+                            <option value="6">6</option>
+                            <option value="5">5</option>
+                            <option value="4">4</option>
+                            <option value="3">3</option>
+                            <option value="2">2</option>
+                            <option value="1">1</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="review">Your review:</label>
+                        <textarea name="review" id="review" rows="3" class="form-control" placeholder="What did you think about the movie?"></textarea>
+                    </div>
+                    <input type="submit" class="btn card-btn" value="Submit Review">
+                </form>
+             </div>
+             <!--- Reviews -->
+             <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-1">
+                        <div class="profile-image-container review-image" style="background-image: url('<?= $BASE_URL ?>img/users/user.png"></div>
+                    </div>
+                    <div class="col-md-9 author-details-container">
+                        <h4 class="author-name">
+                            <a href="#">Luis Test</a>
+                        </h4>
+                        <p><i class="fas fa-star"></i>9 </p>
+                    </div>
+                    <div class="col-md-12">
+                        <p class="comment-title">Review:</p>
+                        <p>User review</p>
+                    </div>
+                </div>
+             </div>
+             <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-1">
+                        <div class="profile-image-container review-image" style="background-image: url('<?= $BASE_URL ?>img/users/user.png"></div>
+                    </div>
+                    <div class="col-md-9 author-details-container">
+                        <h4 class="author-name">
+                            <a href="#">Luis Test</a>
+                        </h4>
+                        <p><i class="fas fa-star"></i>9 </p>
+                    </div>
+                    <div class="col-md-12">
+                        <p class="comment-title">Review:</p>
+                        <p>User review</p>
+                    </div>
+                </div>
+             </div>
+             <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-1">
+                        <div class="profile-image-container review-image" style="background-image: url('<?= $BASE_URL ?>img/users/user.png"></div>
+                    </div>
+                    <div class="col-md-9 author-details-container">
+                        <h4 class="author-name">
+                            <a href="#">Luis Test</a>
+                        </h4>
+                        <p><i class="fas fa-star"></i>9 </p>
+                    </div>
+                    <div class="col-md-12">
+                        <p class="comment-title">Review:</p>
+                        <p>User review</p>
+                    </div>
+                </div>
+             </div>
+        </div>
+    </div>
+</div>
+
+<?php 
+    require_once("templates/footer.php");
 ?>
